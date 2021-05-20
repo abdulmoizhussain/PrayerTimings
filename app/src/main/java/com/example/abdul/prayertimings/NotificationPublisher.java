5d1646979789a36a3dfead817768f2f1c170424c
Abdul Moiz <moiz@prosperdigital.com.au>
2021-05-20 20:27:34 +0500

    removed the code which was checking the duration of 3000 millis then sending the notification..

--- app/src/main/java/com/example/abdul/prayertimings/NotificationPublisher.java
+++ app/src/main/java/com/example/abdul/prayertimings/NotificationPublisher.java
@@ -3,35 +3,17 @@ package com.example.abdul.prayertimings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
-
-import java.text.ParseException;
-import java.util.Date;
 
 public class NotificationPublisher extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Global.getNotificationFlag(context)) {
-            int notificationId = intent.getIntExtra("index", 0);
-            long setWhen = intent.getLongExtra("setWhen", 0);
-            String contentText = intent.getStringExtra("MSG");
-
-            try {
-                Date currentSystemTime = DateFormats.hour24.parse(new DateTime().formatIn24Hour());
-                Date timeToCheck = DateFormats.hour24.parse(DateFormats.hour24.format(setWhen));
-                Date timeToCheck1 = DateFormats.hour24.parse(DateFormats.hour24.format(setWhen + 3000));
-
-                assert currentSystemTime != null;
-                if (currentSystemTime.equals(timeToCheck) || currentSystemTime.equals(timeToCheck1)) {
-                    NotificationManagement.publishNotification(context, notificationId, contentText, setWhen);
-                }
-            } catch (ParseException e) {
-                NotificationManagement.notifyWithErrorDetails(context, e.toString());
-                e.printStackTrace();
-                Toast.makeText(context, "Unable to parse time while publishing notification.", Toast.LENGTH_LONG).show();
-            }
+        if (!Global.getNotificationFlag(context)) {
+            return;
         }
+
+        int notificationId = intent.getIntExtra("index", 0);
+        long setWhen = intent.getLongExtra("setWhen", 0);
+        String contentText = intent.getStringExtra("MSG");
+        NotificationManagement.publishNotification(context, notificationId, contentText, setWhen);
     }
-
-
 }
